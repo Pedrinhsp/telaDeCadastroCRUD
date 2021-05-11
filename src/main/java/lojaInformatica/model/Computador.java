2f21b778ca5cd7de8f4be80edb1fc9f9344a56a3
Pedro <pedrinho.top07@gmail.com>
2021-05-11 17:59:46 -0300

    adicionado o arquivo de controller da aplicação

--- src/main/java/lojaInformatica/model/Computador.java
+++ src/main/java/lojaInformatica/model/Computador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lojainformatica.model;
 
 /**
@@ -10,11 +5,6 @@ package lojainformatica.model;
  * @author Pedrin
  */
 public class Computador {
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
     
     private static String marca = "Pedro Hen";
     private int Id;
